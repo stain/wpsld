50afad62a1e44f244942128511b712b51ee8a569
Stian Soiland-Reyes <stain@apache.org>
2021-05-31 19:39:10 +0100

    ignores

---
+++ lib/src/main/java/no/s11/wpsld/WPSLD.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 The University of Manchester, UK
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package no.s11.wpsld;
+
+import java.io.IOException;
+
+/**
+ * Entry service for WpsLD
+ */
+public interface WPSLD {
+
+	/**
+	 * Create a temporary WPSLD root folder.
+	 * 
+	 * @return A new, empty WPSLD root path
+	 * @throws IOException If the temporary directory could not be created 
+	 */
+	WPSLDPath newRoot() throws IOException;
+
+	
+}
